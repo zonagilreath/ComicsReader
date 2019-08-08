2c065d4068d4d3ddb88c075de0cd77c5383e4926
zonagilreath <bryson.gilreath@gmail.com>
2019-08-07 20:13:10 -0500

    successfully receiving and piping image data to db

--- db/index.js
+++ db/index.js
@@ -18,50 +18,73 @@ const dbURL = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${pr
 
 module.exports = {};
 
-// db.connect()
-// .then(obj => {
-//   console.log('connected to psql on port', process.env.PGPORT)
-//   obj.done(); // success, release the connection;
-// })
-// .catch(error => {
-//   console.log('ERROR:', error.message || error);
-// });
-// 
-// db.tx(tx => {
-//   const man = new LargeObjectManager({pgPromise: tx});
-//  
-//   // If you are on a high latency connection and working with
-//   // large LargeObjects, you should increase the buffer size.
-//   // The buffer should be divisible by 2048 for best performance
-//   // (2048 is the default page size in PostgreSQL, see LOBLKSIZE)
-//   const bufferSize = 16384;
-//  
-//   return man.createAndWritableStreamAsync(bufferSize)
-//   .then(([oid, stream]) => {
-//     // The server has generated an oid
-//     console.log('Creating a large object with the oid', oid);
-//  
-//     const fileStream = createReadStream(path.resolve(__dirname, '../Star Wars 070 (2019)', 'Star Wars 070-000.jpg'));
-//     fileStream.pipe(stream);
-//  
-//     return new Promise((resolve, reject) => {
-//       stream.on('finish', resolve);
-//       stream.on('error', reject);
-//     });
-//   });
-// })
-// .then(() => {
-//   console.log('Done!');
-// })
-// .catch(error => {
-//   console.log('Something went horribly wrong!', error);
-// });
-// 
+ // db.connect()
+ // .then(obj => {
+ //   console.log('connected to psql on port', process.env.PGPORT)
+ //   obj.done(); // success, release the connection;
+ // })
+ // .catch(error => {
+ //   console.log('ERROR:', error.message || error);
+ // });
+ 
+ // db.tx(tx => {
+ //  const man = new LargeObjectManager({pgPromise: tx});
+ // 
+ //  // If you are on a high latency connection and working with
+ //  // large LargeObjects, you should increase the buffer size.
+ //  // The buffer should be divisible by 2048 for best performance
+ //  // (2048 is the default page size in PostgreSQL, see LOBLKSIZE)
+ //  const bufferSize = 16384;
+ // 
+ //  return man.createAndWritableStreamAsync(bufferSize)
+ //  .then(([oid, stream]) => {
+ //    // The server has generated an oid
+ //    console.log('Creating a large object with the oid', oid);
+ // 
+ //    const fileStream = createReadStream(path.resolve(__dirname, '../Star Wars 070 (2019)', 'Star Wars 070-000.jpg'));
+ //    fileStream.pipe(stream);
+ // 
+ //    return new Promise((resolve, reject) => {
+ //      stream.on('finish', resolve);
+ //      stream.on('error', reject);
+ //    });
+ //  });
+ // })
+ // .then(() => {
+ //  console.log('Done!');
+ // })
+ // .catch(error => {
+ //  console.log('Something went horribly wrong!', error);
+ // });
+
+module.exports.postImage = (file) => {
+
+  db.tx(tx => {
+    const man = new LargeObjectManager({pgPromise: tx});
+    const bufferSize = 16384;
+
+    return man.createAndWritableStreamAsync(bufferSize)
+    .then(([oid, stream]) => {
+      console.log('Creating a lob with oid:', oid);
+      file.pipe(stream)
+      // file.on('data', (data)=>{
+      //   console.log('got some data boss!', data);
+      //   data.pipe(stream)
+      // })
+
+      return new Promise((resolve, reject) => {
+        stream.on('finish', resolve);
+        stream.on('error', reject);
+      });
+    });
+  })
+}
+
+// test oid = 16399
 
 module.exports.getImage = (res, oid) => {
   return db.tx(tx => {
     const man = new LargeObjectManager({pgPromise: tx});
-    // const oid = 16399;
     const bufferSize = 16384;
    
     return man.openAndReadableStreamAsync(oid, bufferSize)
@@ -74,43 +97,4 @@ module.exports.getImage = (res, oid) => {
       });
     })
   })
- // .then(() => {
-   // console.log('Done!');
- // })
- // .catch(error => {
-   // console.log('Something went horribly wrong!', error);
- // });
 }
-
- // db.tx(tx => {
- //   const man = new LargeObjectManager({pgPromise: tx});
- //  
- //   // A LargeObject oid, probably stored somewhere in one of your own tables.
- //   const oid = 16399;
- //  
- //   // If you are on a high latency connection and working with
- //   // large LargeObjects, you should increase the buffer size.
- //   // The buffer should be divisible by 2048 for best performance
- //   // (2048 is the default page size in PostgreSQL, see LOBLKSIZE)
- //   const bufferSize = 16384;
- //  
- //   return man.openAndReadableStreamAsync(oid, bufferSize)
- //   .then(([size, stream]) => {
- //     console.log('Streaming a large object with a total size of', size);
- //  
- //     // Store it as an image
- //     const fileStream = createWriteStream('my-file.jpg');
- //     stream.pipe(fileStream);
- //  
- //     return new Promise((resolve, reject) => {
- //       stream.on('end', resolve);
- //       stream.on('error', reject);
- //     });
- //   });
- // })
- //  .then(() => {
- //    console.log('Done!');
- //  })
- //  .catch(error => {
- //    console.log('Something went horribly wrong!', error);
- //  });
--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const connect = require('connect');
 const app = connect();
 const fs = require('fs');
 const db = require('../db/index.js');
+const Busboy = require('busboy');
+const multer = require('multer');
+const multiparty = require('multiparty');
 
 const dir = path.join(__dirname, 'public');
 
@@ -14,44 +17,66 @@ const mime = {
 };
 
 app.use(function (req, res) {
-  if (req.method !== 'GET') {
-    res.statusCode = 501;
-    res.setHeader('Content-Type', 'text/plain');
-    return res.end('Method not implemented');
+
+  if (req.method === 'POST'){
+    // const form = new multiparty.Form();
+    // form.on('error', function(err) {
+    //   console.log('Error parsing form: ' + err.stack);
+    // });
+    // form.on('part', function(part) {
+    //   if (!part.filename) {
+    //     console.log('got field named ' + part.name);
+    //     part.resume();
+    //   }
+    //  
+    //   if (part.filename) {
+    //     console.log('got file named ' + part.name);
+    //     db.postImage(part);
+    //   }
+    //   part.on('error', function(err) {
+    //     console.error(err)
+    //   });
+    // });
+    // form.on('close', function() {
+    //   console.log('Upload completed!');
+    //   res.setHeader('Content-Type', 'text/plain');
+    //   res.end('Received ' + count + ' files');
+    // });
+    // form.parse(req);
   }
-
-
-
-  const reqpath = req.url.toString().split('?')[0];
-  console.log(reqpath);
-  if (reqpath === '/') {
-    const file = path.join(dir, '/index.html');
-    console.log(file);
-    const type = 'text/html';
-    const static = fs.createReadStream(file);
-    static.on('open', function () {
-        res.setHeader('Content-Type', type);
+  else if (req.method === 'GET'){
+    const reqpath = req.url.toString().split('?')[0];
+    console.log(reqpath);
+    if (reqpath === '/') {
+      const file = path.join(dir, '/index.html');
+      const static = fs.createReadStream(file);
+      static.on('open', function () {
+        res.setHeader('Content-Type', 'text/html');
         static.pipe(res);
-    });
-    static.on('error', function () {
+      });
+      static.on('error', function () {
         res.setHeader('Content-Type', 'text/plain');
         res.statusCode = 404;
         res.end('Not found');
-    });
-  }else{
-    res.setHeader('Content-Type', 'image/jpeg');
-    db.getImage(res, 16399)
-    .then(() => {
-      console.log('Done!');
-    })
-    .catch(error => {
-      console.log('Something went horribly wrong!', error);
-    });
+      });
+    }else{
+      const [_, oid, ext] = reqpath.split(/[\s.\/]+/)
+      const type = mime[ext] || 'text/plain';
+      res.setHeader('Content-Type', type);
+      // const oid = reqpath.split(/[\s.\/]+/)[1];
+      db.getImage(res, oid)
+      .then(() => {
+        console.log('Done!');
+      })
+      .catch(error => {
+        console.log('Something went wrong!', error);
+      });
+    }
   }
 });
 
 app.listen(8080, function () {
-    console.log('Listening on http://localhost:8080/');
+    console.log('Listening on localhost:8080/');
 });
 
 
@@ -61,3 +86,21 @@ app.listen(8080, function () {
 //   return res.end('Forbidden');
 // }
 // const type = mime[path.extname(file).slice(1)] || 'text/plain';
+
+  // if (req.method !== 'GET') {
+  //   res.statusCode = 501;
+  //   res.setHeader('Content-Type', 'text/plain');
+  //   return res.end('Method not implemented');
+  // }
+    // console.log('got a post request');
+    // const busboy = new Busboy({ headers: req.headers });
+    // busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+    //   file.pipe(db.postImage)
+    //   file.on('end', function() {
+    //     console.log('File [' + fieldname + '] Finished');
+    //   });
+    // });
+    // busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+    //   console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+    // });
+    // req.pipe(busboy)
